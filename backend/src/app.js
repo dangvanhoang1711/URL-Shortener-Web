9c3c2f2c700b694b90b08f2e64e6390a61f4ef93
dangvanhoang1711 <dangvanhoang1711@gmail.com>
2026-05-14 19:02:24 +0700

    fix trust proxy for Render

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 
